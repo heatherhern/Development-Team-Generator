6fbd364c94301272f2a1ad13cf051121ab8049cc
Heather Hern <heathernewlin1@gmail.com>
2020-10-14 16:30:51 -0400

    removed htmlRenderer
    duplicate

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,6 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const render = require('./lib/htmlRenderer');
 const path = require("path");
 const fs = require("fs");
 
