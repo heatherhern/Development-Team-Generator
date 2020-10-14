aeef83a5e12f60fedda32185400101ef1fefa634
Heather Hern <heathernewlin1@gmail.com>
2020-10-14 16:30:13 -0400

    require htmlRenderer

--- Develop/app.js
+++ Develop/app.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const render = require('./lib/htmlRenderer');
 const path = require("path");
 const fs = require("fs");
 
